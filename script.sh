627307af0fb42fef40bacf7128ece9c60d1b8b6c
Davina Lowe <krelledavina9260@gmail.com>
2023-02-09 23:36:58 -0500

    adding a script

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+#Update packages 
+sudo apt update && upgrade
+
+#Installing docker 
+sudo apt install docker.io -y
+
+#Check docker version
+sudo docker -v
+sleep 4
+
+#Installing docker compose
+sudo mkdir -p ~/.docker/cli-plugins/
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+
+#Check docker compose version
+sudo docker-compose --version
