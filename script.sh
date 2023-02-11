76fbf6657cfebc7ed88abd4a74b03f5b7e8bf44b
Davina Lowe <krelledavina9260@gmail.com>
2023-02-10 19:54:37 -0500

    adding a script

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ sleep 4
 #Installing docker compose
 sudo mkdir -p ~/.docker/cli-plugins/
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 #Check docker compose version
 sudo docker-compose --version
